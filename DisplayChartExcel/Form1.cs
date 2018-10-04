441332c12ec06bad5a353adcecfdb084691271d2
pkmitl205 <58030205@kmitl.ac.th>
2018-10-04 11:18:36 +0700

    04102561 - 11:20

--- DisplayChartExcel/Form1.cs
+++ DisplayChartExcel/Form1.cs
@@ -16,5 +16,10 @@ namespace DisplayChartExcel
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
